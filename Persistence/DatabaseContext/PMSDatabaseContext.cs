cfb04c0c9f31131f1c08e030b02d14901aaa4fb6
KEMARNORTON <kemarnorton@hotmail.com>
2024-07-09 21:19:09 -0500

    Add migration

--- Persistence/DatabaseContext/PMSDatabaseContext.cs
+++ Persistence/DatabaseContext/PMSDatabaseContext.cs
@@ -29,61 +29,12 @@ public class PMSDatabaseContext : IdentityDbContext<ApplicationUser>
 
     public DbSet<FormInteractionLog> FormInteractionLogs { get; set; }
 
-    //public DbSet<PurposeOfValuationCheckBox> PurposeOfValuationCheckBoxes { get; set; }
-    //public DbSet<PurposeOfValuationCheckBoxProperty> PurposeOfValuationCheckBoxProperties { get; set; }
-
-    //public DbSet<ServiceRequestCheckBox> ServiceRequestCheckBoxes { get; set; }
-    //public DbSet<ServiceRequestCheckBoxProperty> ServiceRequestCheckBoxProperties { get; set; }
-        
-    //public DbSet<TypeOfPropertyCheckBox> TypeOfPropertyCheckBoxes { get; set; }
-    //public DbSet<TypeOfPropertyCheckBoxProperty> TypeOfPropertyCheckBoxProperties { get; set; }
-
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
         builder.ApplyConfiguration(new RolesSeedConfiguration());
         builder.ApplyConfiguration(new UserSeedConfiguration());
         builder.ApplyConfiguration(new UserRolesSeedConfiguration());
-        //builder.ApplyConfiguration(new PurposeOfValuationPropertySeedConfiguration());
-        //builder.ApplyConfiguration(new ServiceRequestPropertySeedConfiguration());
-        //builder.ApplyConfiguration(new TypeOfPropertySeedConfiguration());
-
-        ///Purpose Of Valuation checkboxes Configuration
-        //builder.Entity<PurposeOfValuationCheckBox>().HasKey(p => new { p.FormId, p.PurposeOfValuationCheckBoxPropertyId });
-        //builder.Entity<PurposeOfValuationCheckBox>()
-        //    .HasOne(p => p.Form)
-        //    .WithMany(p => p.PurposeOfValuationCheckBoxItems)
-        //    .HasForeignKey(p => p.FormId);
-
-        //builder.Entity<PurposeOfValuationCheckBox>()
-        //    .HasOne(p => p.PurposeOfValuationCheckBoxProperty)
-        //    .WithMany(p => p.PurposeOfValuationItemCheckBoxes)
-        //    .HasForeignKey(p => p.PurposeOfValuationCheckBoxPropertyId);
-
-        ////Service Request checkboxes Configuration
-        //builder.Entity<ServiceRequestCheckBox>().HasKey(s => new { s.FormId, s.ServiceRequestCheckBoxPropertyId });
-        //builder.Entity<ServiceRequestCheckBox>()
-        //    .HasOne(s => s.Form)
-        //    .WithMany(s => s.ServiceRequestCheckBoxItems)
-        //    .HasForeignKey(s => s.FormId);
-
-        //builder.Entity<ServiceRequestCheckBox>()
-        //    .HasOne(s => s.ServiceRequestCheckBoxProperty)
-        //    .WithMany(s => s.ServiceRequestCheckBoxes)
-        //    .HasForeignKey(s => s.ServiceRequestCheckBoxPropertyId);
-
-        ////Type Of Property checkboxes Configuration
-        //builder.Entity<TypeOfPropertyCheckBox>().HasKey(t => new { t.FormId, t.TypeOfPropertyCheckBoxPropertyId });
-        //builder.Entity<TypeOfPropertyCheckBox>()
-        //    .HasOne(t => t.Form)
-        //    .WithMany(t => t.TypeOfPropertyCheckBoxItems)
-        //    .HasForeignKey(sr => sr.FormId);
-
-        //builder.Entity<TypeOfPropertyCheckBox>()
-        //    .HasOne(t => t.TypeOfPropertyCheckProperty)
-        //    .WithMany(t => t.TypeOfPropertyCheckBoxes)
-        //    .HasForeignKey(t => t.TypeOfPropertyCheckBoxPropertyId);
-
-
+        
     }
 }
