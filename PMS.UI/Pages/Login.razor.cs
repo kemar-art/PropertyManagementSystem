ae39af245385f2473eacaca79bac48ac4ccc0156
KEMARNORTON <kemarnorton@hotmail.com>
2024-08-04 15:23:15 -0500

    Impolement login page

--- PMS.UI/Pages/Login.razor.cs
+++ PMS.UI/Pages/Login.razor.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using MudBlazor;
 using PMS.UI.Contracts.Repository_Interface;
 using PMS.UI.Models.Atuh;
+using PMS.UI.Models.Auth;
+using System.Threading.Tasks;
 
 namespace PMS.UI.Pages
 {
     public partial class Login
     {
-        public LoginVM  LoginVM { get; set; }
+        public LoginVM LoginVM { get; set; }
 
         [Inject]
         public NavigationManager NavigationManager { get; set; }
@@ -15,14 +19,13 @@ namespace PMS.UI.Pages
         [Inject]
         private IAuthenticationService AuthenticationService { get; set; }
 
-        public Login()
-        {
-
-        }
+        private bool IsLoading { get; set; } = true;
 
         protected override void OnInitialized()
         {
+            IsLoading = true;
             LoginVM = new LoginVM();
+            IsLoading = false;
         }
 
         protected async Task HandleLogin()
@@ -34,4 +37,92 @@ namespace PMS.UI.Pages
             Message = "Username/password combination unknown";
         }
     }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+//using Microsoft.AspNetCore.Components;
+//using Microsoft.AspNetCore.WebUtilities;
+//using PMS.UI.Contracts.Repository_Interface;
+//using PMS.UI.Models.Atuh;
+//using PMS.UI.Models.Auth;
+//using System.Web;
+
+//namespace PMS.UI.Pages
+//{
+//    public partial class Register
+//    {
+//        public RegisterVM RegisterVM { get; set; } = new();
+
+//        [Inject]
+//        public NavigationManager _NavigationManager { get; set; }
+
+//        public string Message { get; set; }
+
+//        public bool EmailExists { get; set; }
+
+//        [Inject]
+//        private IAuthenticationService _AuthenticationService { get; set; }
+
+//        protected override void OnInitialized()
+//        {
+//            var uri = _NavigationManager.ToAbsoluteUri(_NavigationManager.Uri);
+//            if (QueryHelpers.ParseQuery(uri.Query).TryGetValue("firstName", out var firstName))
+//            {
+//                RegisterVM.FirstName = firstName;
+//            }
+//            if (QueryHelpers.ParseQuery(uri.Query).TryGetValue("lastName", out var lastName))
+//            {
+//                RegisterVM.LastName = lastName;
+//            }
+//            if (QueryHelpers.ParseQuery(uri.Query).TryGetValue("email", out var email))
+//            {
+//                RegisterVM.Email = email;
+//            }
+//            if (QueryHelpers.ParseQuery(uri.Query).TryGetValue("phoneNumber", out var phoneNumber))
+//            {
+//                RegisterVM.PhoneNumber = phoneNumber;
+//            }
+//        }
+
+//        private async Task CheckEmailExistence()
+//        {
+//            if (!string.IsNullOrEmpty(RegisterVM.Email))
+//            {
+//                EmailExists = await _AuthenticationService.IsEmailRegisteredExist(RegisterVM.Email);
+//            }
+//        }
+
+//        protected async Task HandleRegister()
+//        {
+//            if (EmailExists)
+//            {
+//                Message = "Email already exists.";
+//                return;
+//            }
+
+//            var result = await _AuthenticationService.IsRegister(RegisterVM);
+
+//            if (result)
+//            {
+//                _NavigationManager.NavigateTo("/");
+//            }
+//            else
+//            {
+//                Message = "Something went wrong, please try again.";
+//                return;
+//            }
+//        }
+//    }
+//}
