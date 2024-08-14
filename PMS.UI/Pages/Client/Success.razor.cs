ec39bc37378fbf0cee7eb0d8b06d8657a21f8df6
KEMARNORTON <kemarnorton@hotmail.com>
2024-08-14 12:49:45 -0500

    Fix success page for auth users

--- PMS.UI/Pages/Client/Success.razor.cs
+++ PMS.UI/Pages/Client/Success.razor.cs
@@ -1,28 +1,46 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using PMS.UI.Contracts;
 using PMS.UI.Models.Form;
+using System.Security.Claims;
 
 namespace PMS.UI.Pages.Client
 {
     public partial class Success
     {
         [Inject]
-        private IFormRepository _FormRepository { get; set; }
+        private IFormRepository _FormRepository { get; set; } 
 
         [Inject]
         private NavigationManager _NavigationManager { get; set; }
 
         [Inject]
+        private AuthenticationStateProvider _AuthenticationStateProvider { get; set; }
+
+        [Inject]
         private IMapper _Mapper { get; set; }
 
-        public FormVM _successModel { get; set; }
+        public FormVM _successModel { get; set; } = new();
+
+        public ClaimsPrincipal UserAuthstate { get; set; } = new();
+
+        
 
         [Parameter]
         public Guid FormId { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
+            var authState = await _AuthenticationStateProvider.GetAuthenticationStateAsync();
+            UserAuthstate = authState.User;
+             
+            //if (user.Identity.IsAuthenticated)
+            //{
+            //    _NavigationManager.NavigateTo("/create/");
+            //}
+
             if (FormId == Guid.Empty)
             {
                 _NavigationManager.NavigateTo("/error");
