8c69bb3ad1c674057752c4c5a2d41135656a0d7d
KEMARNORTON <kemarnorton@hotmail.com>
2024-09-05 23:32:58 -0500

    Add properties to Admin user

--- PMS.UI/Models/Employee/ApplicationUserVM.cs
+++ PMS.UI/Models/Employee/ApplicationUserVM.cs
@@ -6,25 +6,23 @@ namespace PMS.UI.Models.Employee
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
         public string TaxRegistrationNumber { get; set; } = string.Empty;
         public string NationalInsuranceScheme { get; set; } = string.Empty;
         public string Gender { get; set; } = string.Empty;
         public string ImagePath { get; set; } = string.Empty;
         //public IFormFile? Image { get; set; }
-        [DataType(DataType.Date), Display(Name = ("D.O.B"))]
-        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+        public DateTime? DateOfBirth { get; set; } = DateTime.Now;
 
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
-        [DataType(DataType.Date), Display(Name = ("Start Date"))]
-        public DateTime DateRegistered { get; set; } = DateTime.Now;
+        
+        public DateTime? DateRegistered { get; set; } = DateTime.Now;
 
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
-        [DataType(DataType.Date)]
-        [Display(Name = "End Date")]
-        public DateTime DateEnded { get; set; } = DateTime.Now;
+      
+        public DateTime? DateEnded { get; set; } = DateTime.Now;
 
-        //public string Role { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
 
         //public bool IsActive { get; set; } = true;
 
