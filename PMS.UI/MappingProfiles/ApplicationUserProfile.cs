693afc4480cf3d82ef541d12a5ceda1876415c77
KEMARNORTON <kemarnorton@hotmail.com>
2024-09-12 22:01:09 -0500

    Implement delete for back office users

--- PMS.UI/MappingProfiles/ApplicationUserProfile.cs
+++ PMS.UI/MappingProfiles/ApplicationUserProfile.cs
@@ -15,39 +15,34 @@ public class ApplicationUserProfile : Profile
             .ForMember(dest => dest.DateRegistered, opt => opt.MapFrom(src => src.DateRegistered.Date))
             .ForMember(dest => dest.DateEnded, opt => opt.MapFrom(src => src.DateEnded.Date))
             .ReverseMap();
-
         CreateMap<GetAllClientUsersDTO, ClientVM>()
             .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth.Date))
-            //.ForMember(dest => dest.DateRegistered, opt => opt.MapFrom(src => src.DateRegistered.Date))
             .ReverseMap();
-        //CreateMap<ApplicationUserVM, GetFormDetailsDto>().ReverseMap();
 
         CreateMap<GetASingleUserDTO, ClientVM>()
            .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth.DateTime))
-           //.ForMember(dest => dest.ImagePath, opt => opt.MapFrom(src => src.ImagePath))
            .ReverseMap();
+        CreateMap<GetASingleUserDTO, ApplicationUserVM>()
+            .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth.DateTime))
+            .ForMember(dest => dest.DateRegistered, opt => opt.MapFrom(src => src.DateRegistered.DateTime))
+            .ForMember(dest => dest.DateEnded, opt => opt.MapFrom(src => src.DateEnded.DateTime))
+            .ReverseMap();
 
 
         CreateMap<ClientUpdateCommand, ClientVM>()
            .ReverseMap();
 
-        CreateMap<UpdateBackOfficeUserCommand, ApplicationUserVM>()
-            .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth.DateTime))
-            .ForMember(dest => dest.DateRegistered, opt => opt.MapFrom(src => src.DateRegistered.DateTime))
-            .ForMember(dest => dest.DateEnded, opt => opt.MapFrom(src => src.DateEnded.DateTime))
-           .ReverseMap();
 
         CreateMap<CreateBackOfficeUserCommand, ApplicationUserVM>()
             .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth.DateTime))
             .ForMember(dest => dest.DateRegistered, opt => opt.MapFrom(src => src.DateRegistered.DateTime))
             .ForMember(dest => dest.DateEnded, opt => opt.MapFrom(src => src.DateEnded.DateTime))
            .ReverseMap();
-
-
-        CreateMap<GetASingleUserDTO, ApplicationUserVM>()
+        CreateMap<UpdateBackOfficeUserCommand, ApplicationUserVM>()
             .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth.DateTime))
             .ForMember(dest => dest.DateRegistered, opt => opt.MapFrom(src => src.DateRegistered.DateTime))
             .ForMember(dest => dest.DateEnded, opt => opt.MapFrom(src => src.DateEnded.DateTime))
-            .ReverseMap();
+           .ReverseMap();
+
     }
 }
