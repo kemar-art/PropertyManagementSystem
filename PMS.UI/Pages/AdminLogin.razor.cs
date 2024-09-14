0a61e86661a78e94c9834f20f244c263ded33723
KEMARNORTON <kemarnorton@hotmail.com>
2024-09-14 14:36:22 -0500

    Add amin login page

---
+++ PMS.UI/Pages/AdminLogin.razor.cs
@@ -0,0 +1,15 @@
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using PMS.UI.Contracts.Repository_Interface;
+using PMS.UI.Models.Atuh;
+using PMS.UI.Models.Auth;
+using System.Threading.Tasks;
+
+namespace PMS.UI.Pages
+{
+    public partial class Login
+    {
+    }
+}
