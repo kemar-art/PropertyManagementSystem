91a83f689bad2e4b6af0c451670e1008a014328f
KEMARNORTON <kemarnorton@hotmail.com>
2024-09-15 14:30:15 -0500

    Add code for admin login page

--- PMS.UI/Pages/AdminLogin.razor.cs
+++ PMS.UI/Pages/AdminLogin.razor.cs
@@ -1,6 +1,7 @@
 using Blazored.LocalStorage;
 using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
 using MudBlazor;
 using PMS.UI.Contracts.Repository_Interface;
 using PMS.UI.Models.Atuh;
@@ -11,5 +12,27 @@ namespace PMS.UI.Pages
 {
     public partial class Login
     {
+        public LoginVM loginModel { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            loginModel = new LoginVM();
+
+
+        }
+
+
+
+        protected async Task OnValidSubmit()
+        {
+            // Handle login
+        }
+
+        public class LoginModel
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
