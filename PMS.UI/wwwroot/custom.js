bfd620734cff4631a2536e3c91691b61badef609
KEMARNORTON <kemarnorton@hotmail.com>
2024-10-12 19:15:06 -0500

    Fix bug - When moving to another page there was a JS error

--- PMS.UI/wwwroot/custom.js
+++ PMS.UI/wwwroot/custom.js
@@ -16,14 +16,7 @@ window.reloadVendorScript = function () {
             initializeDropdown(); // Call your dropdown init function once the script is loaded
         };
         document.body.appendChild(script);
-    } else {
-        // If the script is already loaded, just reinitialize the dropdown
-        initializeDropdown();
     }
 };
 
-// Example function inside preload.js to initialize dropdown
-function initializeDropdown() {
-    $('.dropdown-toggle').dropdown(); // If using Bootstrap or similar for dropdowns
-}
 
